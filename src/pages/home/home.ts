53f048dae55a2c923e63ae54f72e1a6b361a10dc
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-03-18 17:35:25 +0530

    commit 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { SpeechRecognition } from "@ionic-native/speech-recognition";
+import { ChangeDetectorRef } from "@angular/core";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  matches: String[];
+  isRecording = false;
 
+  constructor(
+    private navCtrl: NavController,
+    private speech: SpeechRecognition,
+    private plt: Platform,
+    private cd: ChangeDetectorRef
+  ) {
+
+  }
+
+  isIos() {
+    return this.plt.is('ios');
+  }
+
+  getPermission() {
+    this.speech.hasPermission()
+      .then((has: boolean) => {
+        if(!has) {
+          this.speech.requestPermission();
+        }
+      })
+  }
+
+  startListening() {
+    let options = {
+      language: 'en-US'
+    };
+    this.speech.startListening(options).subscribe(matches => {
+      this.matches  = matches;
+      this.cd.detectChanges();
+    });
+    this.isRecording = true;
+  }
+
+  stopListening() {
+    this.speech.stopListening().then(() => {
+      this.isRecording = false;
+    });
   }
 
 }
